d7ce8bb0527974159d94e22eab054473a5c23fb6
Artem Ervits <generic13@gmail.com>
2020-06-15 11:42:55 -0400

    removed arg for user

--- cockroach-secure/up.sh
+++ cockroach-secure/up.sh
@@ -3,7 +3,6 @@
 if [ "$*" == "" ]; then
     echo "No arguments provided"
     echo "Argument 1: databasename"
-    echo "Argument 2: username"
     exit 1
 fi
 
@@ -26,11 +25,11 @@ docker-compose exec roach-0 \
 docker-compose exec roach-0 \
  /cockroach/cockroach sql \
  --certs-dir=/certs --host=roach-0 \
- --execute="CREATE USER $USER WITH PASSWORD 'roach';"
+ --execute="CREATE USER max WITH PASSWORD 'roach';"
 
 docker-compose exec roach-0 \
  /cockroach/cockroach sql \
  --certs-dir=/certs --host=roach-0 \
- --execute="GRANT ALL ON DATABASE $DATABASE TO $USER;"
+ --execute="GRANT ALL ON DATABASE $DATABASE TO max;"
 
 docker-compose exec roach-0 /cockroach/cockroach sql --certs-dir=/certs --host=roach-0 --execute="SET CLUSTER SETTING server.remote_debugging.mode = \"any\";"
