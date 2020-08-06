109ed0f371b55426b931ab192df563ff17a89a2f
Artem Ervits <generic13@gmail.com>
2020-08-06 17:03:54 -0400

    working

--- cockroach-gssapi-django/django/start.sh
+++ cockroach-gssapi-django/django/start.sh
@@ -9,6 +9,6 @@ env
 python manage.py migrate
 
 # Exit with error unless we find the expected error message.
-#python manage.py inspectdb 2>&1 | grep 'use of GSS authentication requires an enterprise license'
+python manage.py inspectdb 2>&1 | grep 'use of GSS authentication requires an enterprise license'
 
 tail /dev/null
