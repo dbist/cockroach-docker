9c9aafd5553ee11ac56e8c1541f7239bee407fcb
Artem Ervits <generic13@gmail.com>
2020-08-07 10:28:51 -0400

    kerberos working again

--- cockroach-gssapi-django/django/start.sh
+++ cockroach-gssapi-django/django/start.sh
@@ -6,7 +6,4 @@ echo psql | kinit tester@EXAMPLE.COM
 
 env
 
-# Exit with error unless we find the expected error message.
-python manage.py inspectdb 2>&1 | grep 'use of GSS authentication requires an enterprise license'
-
-tail /dev/null
+tail -f /dev/null
