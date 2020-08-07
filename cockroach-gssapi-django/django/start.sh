cef943547d0ab2cbf4c5eb2892da398a97a32961
Artem Ervits <generic13@gmail.com>
2020-08-07 12:54:33 -0400

    working

--- cockroach-gssapi-django/django/start.sh
+++ cockroach-gssapi-django/django/start.sh
@@ -5,8 +5,7 @@ set -e
 echo psql | kinit tester@EXAMPLE.COM
 
 env
-echo "waiting a bit"
+echo "sleep 10"
 sleep 10
-python manage.py migrate
 python manage.py runserver 0.0.0.0:9999
-tail -f /dev/null
+#tail -f /dev/null
