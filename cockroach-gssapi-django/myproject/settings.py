e9033d7e83224928929b3cf540f79864e9af737f
Artem Ervits <generic13@gmail.com>
2020-08-13 16:55:32 -0400

    address code smell

--- cockroach-gssapi-django/myproject/settings.py
+++ cockroach-gssapi-django/myproject/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+DJANGO_SETTINGS_MODULE = 'myproject.settings'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
