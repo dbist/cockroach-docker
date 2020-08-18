cd0d3b1e918ea65d14191c2146a4c53dedbb61a3
Artem Ervits <generic13@gmail.com>
2020-08-18 12:04:17 -0400

    pylint errors
    Fixes #65

--- cockroach-gssapi-django/myproject/models.py
+++ cockroach-gssapi-django/myproject/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# pylint: disable=E0611
 
 class Customers(models.Model):
     id = models.AutoField(primary_key=True)
--- cockroach-gssapi-django/myproject/urls.py
+++ cockroach-gssapi-django/myproject/urls.py
@@ -1,3 +1,5 @@
+# pylint: disable=E0611
+
 from django.contrib import admin
 from django.urls import path
 
