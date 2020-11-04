b7fe5a12508b58db3cd179945aa55da6b8c70237
Artem Ervits <generic13@gmail.com>
2020-11-04 11:56:12 -0500

    security is on

--- cockroach-rails/up.sh
+++ cockroach-rails/up.sh
@@ -3,13 +3,24 @@
 docker-compose build --no-cache
 docker-compose up -d
 
-docker-compose exec database \
- /cockroach/cockroach sql \
- --insecure --host=crdb \
- --execute="CREATE DATABASE IF NOT EXISTS rails_development;"
+#docker-compose exec roach-0 \
+# /cockroach/cockroach sql \
+# --certs-dir=/certs --host=roach-0 \
+# --execute="CREATE DATABASE IF NOT EXISTS rails_development;"
 
-docker-compose exec database \
+docker-compose exec roach-0 \
  /cockroach/cockroach sql \
- --insecure --host=crdb \
- --execute="CREATE USER IF NOT EXISTS roach;"
-docker-compose exec app bundle exec rake db:setup db:migrate
+ --certs-dir=/certs --host=roach-0 \
+ --execute="CREATE USER IF NOT EXISTS roach WITH PASSWORD 'roach';"
+
+docker-compose exec roach-0 \
+ /cockroach/cockroach sql \
+ --certs-dir=/certs --host=roach-0 --execute="GRANT ADMIN TO roach;"
+
+#docker-compose exec roach-0 \
+# /cockroach/cockroach sql \
+# --certs-dir=/certs --host=roach-0 \
+# --execute="GRANT ALL ON DATABASE rails_development TO roach;"
+
+docker-compose exec app \
+ bundle exec rake db:setup db:migrate
