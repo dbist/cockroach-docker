bd9335b0b74eb9626786f1459c37c962cb5cb38f
Artem Ervits <generic13@gmail.com>
2021-01-22 14:04:53 -0500

    updated for 20.2 and django 3.1

--- cockroach-django/up.sh
+++ cockroach-django/up.sh
@@ -4,6 +4,10 @@ docker-compose build --no-cache
 docker-compose up -d
 
 docker-compose exec roach-0 \
+ /cockroach/cockroach init \
+ --certs-dir=/certs --host=roach-0
+
+docker-compose exec roach-0 \
  /cockroach/cockroach sql \
  --certs-dir=/certs --host=roach-0 \
  --execute="CREATE DATABASE myproject;"
