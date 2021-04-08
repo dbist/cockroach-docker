65c23551667f0033a5c2db4d5e7d3175ba0f68ee
Artem Ervits <generic13@gmail.com>
2021-04-08 11:17:01 -0400

    insecure pgbouncer example

---
+++ cockroach-pgbouncer/up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker-compose build --no-cache
+docker-compose up -d
+
+docker exec -it roach-0 \
+ /cockroach/cockroach init \
+ --insecure --host=roach-0
+
+docker-compose exec roach-0 \
+ /cockroach/cockroach sql \
+ --insecure --host=roach-0 \
+ --execute="CREATE USER IF NOT EXISTS roach;"
+
+docker-compose exec roach-0 \
+ /cockroach/cockroach sql \
+ --insecure --host=roach-0 --execute="GRANT ADMIN TO roach;"
