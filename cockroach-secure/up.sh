99a79ef01d6082f9200823732e72af89104cfa0d
Artem Ervits <generic13@gmail.com>
2022-01-25 14:44:15 -0500

    removed explicit call to initiale the cluster and replaced it with an init container

--- cockroach-secure/up.sh
+++ cockroach-secure/up.sh
@@ -3,10 +3,6 @@
 docker compose build --no-cache
 docker compose up -d
 
-docker exec -it roach-0 \
- /cockroach/cockroach init \
- --certs-dir=/certs --host=roach-0
-
 docker compose exec roach-0 \
  /cockroach/cockroach sql \
  --certs-dir=/certs --host=roach-0 \
