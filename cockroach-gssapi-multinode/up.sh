58e790da5a96aa09108662ca3b3c345f6fad0a88
Artem Ervits <generic13@gmail.com>
2022-01-31 17:09:32 -0500

    Updates
    1. updated c-ares to fix a 404 error
    2. removed init step in favor of init container
    3. upgraded haproxy

--- cockroach-gssapi-multinode/up.sh
+++ cockroach-gssapi-multinode/up.sh
@@ -6,10 +6,6 @@ docker compose build --no-cache
 docker compose up -d
 
 docker compose exec roach-0 \
- /cockroach/cockroach init \
- --certs-dir=/certs --host=roach-0
-
-docker compose exec roach-0 \
  /cockroach/cockroach sql \
  --certs-dir=/certs --host=roach-0 \
  --execute="CREATE USER tester;"
