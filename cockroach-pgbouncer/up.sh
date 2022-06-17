f1e1bd154a8dc9688d441eafa3246db4297259c7
Artem Ervits <generic13@gmail.com>
2022-06-17 09:26:54 -0400

    updated

--- cockroach-pgbouncer/up.sh
+++ cockroach-pgbouncer/up.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker-compose build --no-cache
-docker-compose up -d
+docker compose build --no-cache
+docker compose up -d
 
-docker-compose exec roach-0 \
+docker compose exec roach-0 \
  /cockroach/cockroach sql \
  --insecure --host=roach-0 \
  --execute="CREATE USER IF NOT EXISTS roach;"
 
-docker-compose exec roach-0 \
+docker compose exec roach-0 \
  /cockroach/cockroach sql \
  --insecure --host=roach-0 --execute="GRANT ADMIN TO roach;"
