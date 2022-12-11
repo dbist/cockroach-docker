69c67ffdd00d9e90c69b82cd0dea19e40bc60ecb
Artem Ervits <generic13@gmail.com>
2022-12-10 19:57:05 -0500

    fix regression

--- cockroach-dotnet/up.sh
+++ cockroach-dotnet/up.sh
@@ -4,6 +4,7 @@ docker compose build --no-cache
 docker compose up -d
 
 docker compose exec roach-0 \
- /cockroach/cockroach sql \
- --insecure \
- --execute="CREATE DATABASE bank;"
+  /cockroach/cockroach sql \
+  --host=lb \
+  --insecure \
+  --execute="CREATE DATABASE bank;"
